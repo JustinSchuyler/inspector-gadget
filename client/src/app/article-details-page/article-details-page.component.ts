425c0056e9cab04bfc6dda37bc49d6d1b253aca8
kevin ringier <kevinringier@gmail.com>
2018-09-20 19:28:14 -0600

    refactor(article details): modify article arrays

--- client/src/app/article-details-page/article-details-page.component.ts
+++ client/src/app/article-details-page/article-details-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Location } from '@angular/common';
 
 import { ArticleSearchService } from '../shared/services/article-search.service';
@@ -12,26 +12,7 @@ import { Article } from '../shared/models/article';
   styleUrls: ['./article-details-page.component.scss']
 })
 export class ArticleDetailsPageComponent implements OnInit {
-  articleDetailForm = this.fb.group({
-    category: ['', Validators.required],
-    tags: this.fb.array([
-      this.fb.control('')
-    ]),
-    title: ['', Validators.required],
-    symptoms: this.fb.array([
-      this.fb.control('')
-    ]),
-    rootCause: ['', Validators.required],
-    fixDescription: ['', Validators.required],
-    fixSteps: ['', Validators.required],
-    diagnosisSteps: ['', Validators.required],
-    ticketURLs: this.fb.array([
-      this.fb.control('')
-    ]),
-    author: ['', Validators.required]
-  });
-
-  article: Article;
+  articleDetailForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
@@ -41,54 +22,88 @@ export class ArticleDetailsPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if (+this.route.snapshot.paramMap.get('id')) {
-      this.getArticle();
-      this.articleDetailForm.patchValue({
-
-        category: this.article.category,
-        title: this.article.title,
-        rootCause: this.article.rootCause,
-        fixDescription: this.article.fixDescription,
-        fixSteps: this.article.fixSteps,
-        diagnosisSteps: this.article.diagnosisSteps,
-        author: this.article.author
-      });
+    this.initializeFormGroup();
+    const id = +this.route.snapshot.paramMap.get('id');
+    
+    if (id) {
+      this.articleSearchService.getArticle(id).subscribe(this.patchFormGroup.bind(this));
     }
+  }
 
+  private initializeFormGroup(): void {
+    this.articleDetailForm = this.fb.group({
+      id: [''],
+      category: ['', Validators.required],
+      tags: this.fb.array([]),
+      title: ['', Validators.required],
+      symptoms: this.fb.array([
+        this.fb.control('')
+      ]),
+      rootCause: ['', Validators.required],
+      fixDescription: ['', Validators.required],
+      fixSteps: ['', Validators.required],
+      diagnosisSteps: ['', Validators.required],
+      ticketURLs: this.fb.array([
+        this.fb.control('')
+      ]),
+      author: ['', Validators.required]
+    });
+  }
+
+  private patchFormGroup(article: Article): void {
+    this.articleDetailForm.patchValue({
+      id: article.id,
+      category: article.category,
+      title: article.title,
+      rootCause: article.rootCause,
+      fixDescription: article.fixDescription,
+      fixSteps: article.fixSteps,
+      diagnosisSteps: article.diagnosisSteps,
+      author: article.author
+    });
+
+    ['tags', 'symptoms', 'ticketURLs'].map(property => this.patchFormArray(article, property));
+  }
+
+  private patchFormArray(article: Article, property: string) {
+    let formArray = this.articleDetailForm.get(property) as FormArray;
+    let controls = article[property].map(value => new FormControl(value));
+
+    this.clearFormArray(formArray);
+    controls.map(control => formArray.push(control));
+  }
+
+  private clearFormArray(formArray: FormArray): void {
+    while (formArray.length > 0) {
+      formArray.removeAt(0);
+    }
   }
 
   goBack(): void {
     this.location.back();
   }
 
-  getArticle(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.articleSearchService.getArticle(id).subscribe(article => this.article = article);
-  }
-
   onSubmit() {
-    // updateArticleForm();
-    console.warn(this.articleDetailForm.value);
-  }
-
-  updateArticleForm() {
-    this.articleDetailForm.patchValue({
-      category: this.article.category,
-      title: this.article.title,
-      rootCause: this.article.rootCause,
-      fixDescription: this.article.fixDescription,
-      fixSteps: this.article.fixSteps,
-      diagnosisSteps: this.article.diagnosisSteps,
-      author: this.article.author
-    });
+    let article = this.articleDetailForm.value as Article;
+    if (article.id) {
+      console.log('Update:', article);
+    }
+    else {
+      console.log('Save:', article);
+    }
   }
 
   get tags() {
     return this.articleDetailForm.get('tags') as FormArray;
   }
 
-  addTag() {
-    this.tags.push(this.fb.control(''));
+  addTag(tagInput: HTMLInputElement): void {
+    let tag = tagInput.value;
+
+    if (tag) {
+      this.tags.push(this.fb.control(tag));
+      tagInput.value = '';
+    }
   }
 
   get symptoms() {
